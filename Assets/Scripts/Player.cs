856347b00cc8aac8d4f269d30a535a2d27c836c2
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-08-01 16:16:08 +0900

    캐릭터 이동 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 moveVector = Vector3.zero;
+
+    [SerializeField]
+    float Speed;
+
+    [SerializeField]
+    BoxCollider boxCollider;
+
+    [SerializeField]
+    Transform mainBGQuadTranform;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMove();
+    }
+
+    void UpdateMove()
+    {
+        if (moveVector.sqrMagnitude == 0)
+            return;
+
+        moveVector = AdjustMoveVector(moveVector);
+
+        transform.position += moveVector;
+    }
+
+    public void ProcessInput(Vector3 moveDirection)
+    {
+        moveVector = moveDirection * Speed * Time.deltaTime;
+    }
+
+    Vector3 AdjustMoveVector(Vector3 moveVector)
+    {
+        Vector3 result = Vector3.zero;
+        result = boxCollider.transform.position + moveVector;
+        Debug.Log(boxCollider.center);
+
+        if (result.x - boxCollider.size.x * 0.5f < -mainBGQuadTranform.localScale.x * 0.5f)
+            moveVector.x = 0;
+        if (result.x + boxCollider.size.x * 0.5f > mainBGQuadTranform.localScale.x * 0.5f)
+            moveVector.x = 0;
+
+        if (result.y - boxCollider.size.y * 0.5f < -mainBGQuadTranform.localScale.y * 0.5f)
+            moveVector.y = 0;
+        if (result.y + boxCollider.size.y * 0.5f > mainBGQuadTranform.localScale.y * 0.5f)
+            moveVector.y = 0;
+
+        return moveVector;
+    }
+}
---
+++ Assets/Scripts/SystemManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemManager : MonoBehaviour
+{
+    static SystemManager instance;
+
+    static public SystemManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("System Manager Error! Singletone Error");
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    [SerializeField]
+    Player player;
+
+    public Player Hero
+    {
+        get
+        {
+            return player;
+        }
+    }
+}
