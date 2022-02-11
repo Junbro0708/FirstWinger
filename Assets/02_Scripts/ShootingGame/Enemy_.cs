dd5179a6e7eef6923798d114cd39c0220f31a761
Junbro0708 <qkrwns708@soongsil.ac.kr>
2022-02-11 22:00:54 +0900

    배경 추가

---
+++ Assets/02_Scripts/ShootingGame/Enemy_.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_ : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 3f;
+
+    [SerializeField]
+    private GameObject explosionFactory;
+
+    private Vector3 dir;
+
+    private void Start()
+    {
+        int randValue = UnityEngine.Random.Range(0, 10);
+
+        if(randValue < 3)
+        {
+            if (GameObject.Find("Player"))
+            {
+                GameObject target = GameObject.Find("Player");
+                dir = target.transform.position - transform.position;
+                dir.Normalize();
+            }
+        }
+        else
+        {
+            dir = Vector3.down;
+        }
+    }
+
+    private void Update()
+    {
+        transform.position += dir * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject explosion = Instantiate(explosionFactory);
+        explosion.transform.position = collision.transform.position;
+
+        Destroy(gameObject);
+        Destroy(collision.gameObject);
+    }
+}
